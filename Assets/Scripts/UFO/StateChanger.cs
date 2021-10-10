627472ccb173da1650a185f8c28d1c870d4dd8c3
Arun-JM <arun.mohindra5159@gmail.com>
2021-10-10 14:46:52 +1100

    Uploading Assg3

---
+++ Assets/Scripts/UFO/StateChanger.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateChanger : MonoBehaviour
+{
+    //private float timeElapsed = 0;
+    //private Animator ufoAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*ufoAnim = GetComponent<Animator>();
+        InvokeRepeating("SetScared", 3f, 15f);
+        InvokeRepeating("SetDead", 6f, 15f);
+        InvokeRepeating("SetRecover", 9f, 15f);
+        InvokeRepeating("SetNormal", 12f, 15f);*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    /*void SetScared()
+    {
+        ufoAnim.SetBool("Scared", true);
+        ufoAnim.SetBool("Normal", false);
+    }
+    void SetDead()
+    {
+        ufoAnim.SetBool("Dead", true);
+        ufoAnim.SetBool("Scared", false);
+    }
+    void SetRecover()
+    {
+        ufoAnim.SetBool("Recover", true);
+        ufoAnim.SetBool("Dead", false);
+    }
+    void SetNormal()
+    {
+        ufoAnim.SetBool("Normal", true);
+        ufoAnim.SetBool("Recover", false);
+    }*/
+}
