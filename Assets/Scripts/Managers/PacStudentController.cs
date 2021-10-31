6ea825746f8c999e3ded50db083336d07a8c6dae
Arun-JM <arun.mohindra5159@gmail.com>
2021-10-31 11:23:50 +1100

    Basic Movement

--- Assets/Scripts/Managers/PacStudentController.cs
+++ Assets/Scripts/Managers/PacStudentController.cs
@@ -10,48 +10,56 @@ public class PacStudentController : MonoBehaviour
     private float timeElapsed;
     private Animator pacAnimator;
     private Tilemap tilemap;
-    private const float duration = 1f;
-    private Vector3Int movement;
+    private const float duration = 5f;
+    private Vector3 movement;
     private Vector3 previousPosition;
     private Vector3Int nextTile;
     // Start is called before the first frame update
     void Start()
     {
-        tilemap = GameObject.FindGameObjectWithTag("Map").GetComponent<Tilemap>();
+        tilemap = GameObject.FindGameObjectWithTag("Map").GetComponentInChildren<Tilemap>();
         pacAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timeElapsed >= duration)
+        if (Input.GetKeyDown(KeyCode.W)) {
+            lastInput = "w";
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            lastInput = "s";
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            lastInput = "a";
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            lastInput = "d";
+        }
+        if (timeElapsed >= duration || movement == Vector3.zero)
         {
             switch (lastInput)
             {
                 case "a":
-                    movement = new Vector3Int(-1, 0, 0);
+                    movement = new Vector3(-0.5f, 0, 0);
                     break;
                 case "d":
-                    movement = new Vector3Int(1, 0, 0);
+                    movement = new Vector3(0.5f, 0, 0);
                     break;
                 case "w":
-                    movement = new Vector3Int(0, 1, 0);
+                    movement = new Vector3(0, 0.5f, 0);
                     break;
                 case "s":
-                    movement = new Vector3Int(0, -1, 0);
+                    movement = new Vector3(0, -0.5f, 0);
                     break;
                 default:
                     break;
             }
-            nextTile = Vector3Int.FloorToInt(transform.position) + movement;
-            if (tilemap.GetSprite(nextTile) != null && (!tilemap.GetSprite(nextTile).name.Equals("RegPellet") || !tilemap.GetSprite(nextTile).name.Equals("PowerPellet")))
-            {
-                movement = new Vector3Int(0, 0, 0);
-            }
+            Debug.Log(nextTile);
             timeElapsed = 0;
             previousPosition = transform.position;
+        } else
+        {
+            float t = timeElapsed / duration;
+            transform.position = Vector3.Lerp(previousPosition, previousPosition + movement, t);
+            timeElapsed += Time.deltaTime;
         }
-        transform.position = Vector3.Lerp(transform.position, nextTile, timeElapsed / duration);
-        timeElapsed += Time.deltaTime;
     }
 }
