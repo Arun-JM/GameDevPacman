d4e05d1bb59821e14818da765f675ea985536686
Arun-JM <arun.mohindra5159@gmail.com>
2021-10-31 13:11:42 +1100

    Movement Sound Transitions

--- Assets/Scripts/Managers/PacStudentController.cs
+++ Assets/Scripts/Managers/PacStudentController.cs
@@ -9,6 +9,8 @@ public class PacStudentController : MonoBehaviour
     private string lastInput;
     private float timeElapsed;
     private Animator pacAnimator;
+    private AudioSource pacAudio;
+    public AudioClip[] movementAudio = new AudioClip[2];
     private Tilemap tilemap;
     private const float duration = 1f;
     private Vector3 movement;
@@ -19,7 +21,7 @@ public class PacStudentController : MonoBehaviour
     {
         tilemap = GameObject.FindGameObjectWithTag("Map").GetComponentInChildren<Tilemap>();
         pacAnimator = GetComponent<Animator>();
-        
+        pacAudio = GetComponentInChildren<AudioSource>();
     }
 
     // Update is called once per frame
@@ -40,15 +42,19 @@ public class PacStudentController : MonoBehaviour
             {
                 case "a":
                     movement = new Vector3(-0.2f, 0, 0);
+                    pacAnimator.SetInteger("Direction", 0);
                     break;
                 case "d":
                     movement = new Vector3(0.2f, 0, 0);
+                    pacAnimator.SetInteger("Direction", 180);
                     break;
                 case "w":
                     movement = new Vector3(0, 0.2f, 0);
+                    pacAnimator.SetInteger("Direction", 90);
                     break;
                 case "s":
                     movement = new Vector3(0, -0.2f, 0);
+                    pacAnimator.SetInteger("Direction", 270);
                     break;
                 default:
                     break;
@@ -59,7 +65,16 @@ public class PacStudentController : MonoBehaviour
             if (tilemap.GetSprite(nextTile) != null && !tilemap.GetSprite(nextTile).name.Equals("RegPellet"))
             {
                 movement = Vector3.zero;
+                pacAudio.Stop();
+                pacAudio.clip = null;
+            } else if (tilemap.GetSprite(nextTile) == null) {
+                if (pacAudio.clip != movementAudio[0]) { pacAudio.clip = movementAudio[0]; pacAudio.Play(); }
+            } else if (tilemap.GetSprite(nextTile).name.Equals("RegPellet"))
+            {
+               pacAudio.clip = movementAudio[1];
+               pacAudio.PlayOneShot(movementAudio[1]);
             }
+            
         } else
         {
             float t = timeElapsed / duration;
